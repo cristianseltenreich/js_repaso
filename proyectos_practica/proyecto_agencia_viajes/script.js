215d360f8b6e05e63db62a386be1b03fd92f5d2d
cristianseltenreich <cristianseltenreich@gmail.com>
2024-12-22 10:47:34 -0300

    proyecto agencia viajes avances finalizado

--- proyectos_practica/proyecto_agencia_viajes/script.js
+++ proyectos_practica/proyecto_agencia_viajes/script.js
@@ -6,6 +6,9 @@ let enlaces = document.querySelectorAll("a");
 
 let tituloElemento = document.getElementById("titulo");
 let subtituloElemento = document.getElementById("subtitulo");
+let imagenElemento = document.getElementById("imagen")
+let subtituloPrecio = document.getElementById("precio")
+let botonComprar = document.getElementById("comprar")
 let parrafoElemento = document.getElementById("parrafo");
 
 //console.log(enlaces);
@@ -28,6 +31,9 @@ enlaces.forEach(function (enlace){
     //mostrar el contenido en el dom
     tituloElemento.innerHTML = contenido.titulo
     subtituloElemento.innerHTML = contenido.subtitulo
+    imagenElemento.src = contenido.imagen;  //mostrar la imagen de la ciudad seleccionada en el DOM  (se puede usar innerHTML o textContent)  //textContent es más seguro ya que evita la inyección de scripts en el DOM
+    subtituloPrecio.innerHTML = contenido.precio;  //mostrar el precio de la ciudad seleccionada en el subtítulo precio en el DOM  (se puede usar innerHTML otextContent)  //textContent es más seguro ya que evita la inyección de scripts en el DOM  //innerHtml puede causar problemas si el contenido tiene etiquetas html, por lo que se recomienda usar textContent en este caso
+    botonComprar.style.display = "block"; // Mostrar el botón
     parrafoElemento.innerHTML = contenido.parrafo;
     })
 });
